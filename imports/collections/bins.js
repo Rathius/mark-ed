febfecbb724d5e276f636456d53df1ae18eaa6e2
Rathius <rathius@icloud.com>
2016-10-28 15:14:42 -0400

    finished implemtation of the bin edotor input area

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -13,6 +13,10 @@ Meteor.methods({
 	
 	'bins.remove': function(bin) {
 		return Bins.remove(bin);
+	},
+	
+	'bins.update': function(bin, content) {
+		return Bins.update(bin._id, { $set: { content } });
 	}
 });
 
